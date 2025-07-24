4eb91286f60cb7c44c9e364021388a391c4e9232
sixtyfourbits <jaehyung545@gmail.com>
2025-07-24 21:31:36 +0900

    .

--- src/app/api/seats/[seat_id]/route.ts
+++ src/app/api/seats/[seat_id]/route.ts
@@ -1,11 +1,10 @@
-
 import { NextRequest, NextResponse } from 'next/server';
 import Database from 'better-sqlite3';
 
 const db = new Database('database.db');
 
-export async function GET(req: NextRequest, context: { params: { seat_id: string } }) {
-  const { seat_id } = context.params;
+export async function GET(request: NextRequest, { params }: { params: { seat_id: string } }) {
+  const seat_id = params.seat_id;
 
   const seat = db.prepare('SELECT * FROM Seats WHERE id = ?').get(seat_id);
 
